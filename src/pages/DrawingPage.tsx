2b422a1d1e67dfe2403476a55e7a48b78c029c38
Sanjai Kumar R <145897247+Sanjai451@users.noreply.github.com>
2025-08-09 15:47:56 +0530

    removed unused variables

--- src/pages/DrawingPage.tsx
+++ src/pages/DrawingPage.tsx
@@ -1,15 +1,7 @@
 import { Tldraw } from '@tldraw/tldraw';
 import 'tldraw/tldraw.css'
-import { useEffect, useState } from 'react';
 
 const DrawingPage = () => {
-    const [roomID, setRoomID] = useState<string | null>('')
-     
-    useEffect(()=>{
-      const id = localStorage.getItem('roomID')
-      setRoomID(id);
-    },[])
-
   return (
     <div className="draw_component" style={{ width: "100%", height: "100%" }}>
     <Tldraw />
